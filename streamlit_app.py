81be920554e444eb11232b84e723126672bcf50c
ooklathemok99 <143349519+ooklathemok99@users.noreply.github.com>
2023-08-27 16:40:03 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 dimport streamlit
 streamlit.title('My name is the real Slim Shady')
-streamlit.header ('line line')
-streamlit.text ('you don't know if you know')
+streamlit.header('line line')
+streamlit.text('you don't know you know')
